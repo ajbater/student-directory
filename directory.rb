fcf2aafe53f2823fb3cfc24f45b75b4b18dbab00
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-25 08:49:18 +0100

    Exercise 9: Using singular form where appropriate, otherwise plural

--- directory.rb
+++ directory.rb
@@ -11,7 +11,11 @@ def input_students
     while !name.empty? do
       # add the student hash to the array
       students << {name: name, cohort: :november}
-      puts "Now we have #{students.count} students"
+        if students.count == 1
+          puts "Now we have #{students.count} student"
+        else
+          puts "Now we have #{students.count} students"
+        end
       # get another name from the user
       name = gets.chomp
     end
@@ -34,7 +38,11 @@ def print(students)
   end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(@line_width)
+  if students.count == 1
+    puts "Overall, we have #{students.count} great student."
+  else
+    puts "Overall, we have #{students.count} great students".center(@line_width)
+  end
 end
 
 students = input_students
