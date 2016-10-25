b3a0b82c80f0403d4849ac638459cd58f0011630
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-25 15:25:16 +0100

    Exercise 8: Sorting the students by cohort (also updating Exercise 7 to account for typos in user input)

--- directory.rb
+++ directory.rb
@@ -5,14 +5,28 @@ def input_students
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
+  cohorts = [
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december"
+  ]
   # get the first names
   puts "Name: "
   name = gets.chomp
   if !name.empty?
     puts "Cohort: "
-    cohort = gets.chomp.to_sym
-    if cohort.empty?
-      cohort = :november
+    cohort = gets.chomp.downcase
+    if cohort.empty? || !cohorts.include?(cohort)
+      cohort = "unknown"
     end
     puts "Country of birth: "
     country_of_birth = gets.chomp
@@ -33,7 +47,7 @@ def input_students
   # while name is not empty, repeat this code
     while !name.empty? do
       # add the student hash to the array
-      students << {name: name, cohort: cohort, country_of_birth: country_of_birth, height: height, hobbies: hobbies}
+      students << {name: name, cohort: cohort.to_sym, country_of_birth: country_of_birth, height: height, hobbies: hobbies}
         if students.count == 1
           puts "Now we have #{students.count} student"
         else
@@ -74,15 +88,25 @@ def print_header
   puts "---------------".center(@line_width)
 end
 
+#def print(students)
+#  counter = 0
+#  until counter == students.length
+#    student = students[counter]
+#    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(@line_width)
+#    puts "Country of birth: #{student[:country_of_birth]}".center(@line_width)
+#    puts "Height: #{student[:height]}".center(@line_width)
+#    puts "Hobbies include: #{student[:hobbies]}".center(@line_width)
+#    counter += 1
+#  end
+#end
+
 def print(students)
-  counter = 0
-  until counter == students.length
-    student = students[counter]
+  sorted_by_cohort = students.sort_by { |hash| hash[:cohort] }
+  sorted_by_cohort.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)".center(@line_width)
     puts "Country of birth: #{student[:country_of_birth]}".center(@line_width)
     puts "Height: #{student[:height]}".center(@line_width)
     puts "Hobbies include: #{student[:hobbies]}".center(@line_width)
-    counter += 1
   end
 end
 
